28017121aa37f2db9fc8f8df567de8a4481b0e3c
Ujjwal <bansalujjwal2002@gmail.com>
2025-04-06 23:55:44 +0530

    setting static files path

--- server.js
+++ server.js
@@ -2,6 +2,9 @@ const express = require("express");
 const mongoose = require("mongoose");
 const cors = require("cors");
 const dotenv = require("dotenv");
+const path = require("path");
+
+// Serve files in the uploads folder statically
 
 dotenv.config();
 const app = express();
@@ -11,6 +14,7 @@ app.use(cors({
   credentials: true,
 }));
 
+app.use("/uploads", express.static(path.join(__dirname, "uploads")));
 app.use(express.json());
 app.use("/uploads", express.static("uploads")); // serve uploaded PDFs
 
